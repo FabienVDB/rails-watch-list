d0830145592f54c4296a9b9b5d361aa0f458d8b7
Fabien VAN DEN BERG <fabvdb@hotmail.com>
2022-05-19 17:04:44 +0200

    Add all routes, controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,32 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# require 'net/http'
-# require 'json'
-# require 'uri'
+require 'net/http'
+require 'json'
+require 'uri'
 
-# puts 'Delete all movies'
-# Movie.destroy_all
+puts 'Delete all movies'
+Movie.destroy_all
 
-# uri_movies = URI('https://tmdb.lewagon.com/movie/top_rated')
-# response_movies = Net::HTTP.get_response(uri_movies)
-# results_movies = JSON.parse(response_movies.body)['results']
+uri_movies = URI('https://tmdb.lewagon.com/movie/top_rated')
+response_movies = Net::HTTP.get_response(uri_movies)
+results_movies = JSON.parse(response_movies.body)['results']
 
-# config_uri = URI('https://tmdb.lewagon.com/configuration')
-# response_config = Net::HTTP.get_response(config_uri)
-# results_config = JSON.parse(response_config.body)
+config_uri = URI('https://tmdb.lewagon.com/configuration')
+response_config = Net::HTTP.get_response(config_uri)
+results_config = JSON.parse(response_config.body)
 
-# base_url = results_config['images']['base_url']
-# poster_size = results_config['images']['poster_sizes'][4] # 'w500'
+base_url = results_config['images']['base_url']
+poster_size = results_config['images']['poster_sizes'][4] # 'w500'
 
-# results_movies.each do |movie|
-#   title = movie['title']
-#   overview = movie['overview']
-#   poster_url = "#{base_url}#{poster_size}#{movie['poster_path']}"
-#   rating = movie['vote_average']
-#   puts "Create movie : #{title}"
-#   Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
-# end
+results_movies.each do |movie|
+  title = movie['title']
+  overview = movie['overview']
+  poster_url = "#{base_url}#{poster_size}#{movie['poster_path']}"
+  rating = movie['vote_average']
+  puts "Create movie : #{title}"
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+end
 
 puts 'Create list'
 List.create(name: 'Drama')
